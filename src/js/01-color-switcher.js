1866bdcacbd016045e97982b11574479ffd3b012
Iryna Kutsevol <117863253+irkutsio@users.noreply.github.com>
2023-04-12 19:03:45 +0300

    timer-create

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,15 +1,57 @@
-function getRandomHexColor() {
-    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
-  }
 
-const logMessage = ()=> {
-    console.log('лог при вызове колбек функции')
-}
 
-console.log('до вызова сет-таймаут')
+//*******************************8 */
+// const logMessage = ()=> {
+//     console.log('лог при вызове колбек функции')
+// }
 
-setTimeout(() => {
-    console.log('внутри колбек для сеттаймаут')
-}, 2000)
+// console.log('до вызова сет-таймаут')
 
-console.log('после вызова сеттаймаут')
+// setTimeout(x => {
+//     console.log('1 - внутри колбек для сеттаймаут')
+//     console.log(x)
+// }, 2000, 'I`am x')
+
+// console.log('после вызова сеттаймаут')
+//*********************** */
+// const logger = time => {
+//     console.log('лог через ${time}, потому что не отменили таймаут')
+// }
+
+// const timerId = setTimeout(logger, 2000, 2000);
+// console.log(timerId)
+
+// const shouldCancelTume = Math.random() > 0.3;
+// console.log(shouldCancelTume);
+
+// // if(shouldCancelTume) {
+// //     clearTimeout(timerId)
+// // }
+
+// const logger = time => console.log(`лог каждые ${time}ms - ${Date.now()}`);
+
+// console.log('до вызова сетинтервал');
+// const intervalId = setInterval(logger, 2000, 2000);
+// console.log('после вызова сетинтервал');
+
+// if (shouldCancelTume) {
+//   clearTimeout(intervalId);
+// }
+
+// *******************************88
+// const notification = document.querySelector('.notification');
+
+// notification.addEventListener('click', onClick)
+
+
+// function onClick (){
+//     hideNotification();
+// }
+
+// setTimeout( ()=>{
+//     notification.classList.add('is-hidden')
+// }, 3000)
+
+// function hideNotification () {
+//     notification.classList.add('is-hidden') 
+// }
