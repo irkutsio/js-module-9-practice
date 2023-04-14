aa60dda115a31e7432c9a43b01fae25b10714458
irkutsio <117863253+irkutsio@users.noreply.github.com>
2023-04-14 16:23:13 +0000

    Deploying to gh-pages from @ irkutsio/js-module-9-practice-@0fb7f29710114148c630d3479bcd10eb4a0141bc 🚀

--- 02-timer.f45bd891.js
+++
@@ -1,2 +0,0 @@
-function t(t){return String(t).padStart(2,"0")}({start(){const o=Date.now();setInterval((()=>{const n=Date.now()-o,{hours:r,minutes:s,seconds:e}=function(o){const n=1e3,r=60*n,s=60*r,e=24*s,a=(Math.floor(o/e),t(Math.floor(o%e/s))),u=t(Math.floor(o%e%s/r)),c=t(Math.floor(o%e%s%r/n));return{hours:a,minutes:u,seconds:c}}(n)}),1e3)}}).start();
-//# sourceMappingURL=02-timer.f45bd891.js.map
