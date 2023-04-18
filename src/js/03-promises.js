8a041d8d0daf759e069c13ec548c1bc6ba262bd2
Iryna Kutsevol <117863253+irkutsio@users.noreply.github.com>
2023-04-18 13:27:21 +0300

    kasino_promises

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -117,8 +117,6 @@
 // console.log("7"); //1,7,3,5,4,2
 //*******************/
 
-
-
 // function createPromise(position, delay, step) {
 //   return new Promise((resolve, reject) => {
 //     setTimeout(() => {
@@ -136,11 +134,11 @@
 
 // form.addEventListener('submit', (event) => {
 //   event.preventDefault();
-  
+
 //   const delay = parseInt(form.elements.delay.value);
 //   const step = parseInt(form.elements.step.value);
 //   const amount = parseInt(form.elements.amount.value);
-  
+
 //   for (let i = 0; i < amount; i++) {
 //     createPromise(i, delay + step * i)
 //       .then(({ position, delay }) => {
@@ -151,3 +149,100 @@
 //       });
 //   }
 // });
+
+//КАЗИНО ВАРІАНТ 1*******************************
+// *************************************************
+
+// const start = document.querySelector('.btn');
+// const container = document.querySelector('.container');
+
+// start.addEventListener('click', onStart);
+
+// function onStart() {
+//     const result = [];
+//   [...container.children].forEach((box) => (box.textContent = ''));
+//   [...container.children].forEach((box, i) => {
+//     createPromise(i)
+//       .then(smile => {
+//         box.textContent = smile;
+//         result.push('1')
+//       })
+//       .catch(smile => {
+//         box.textContent = smile;
+//       })
+//       .finally(() => {
+//        setTimeout(()=>{
+//         if(i === container.children.length-1) {
+//             if (!result.length || result.length === 3) {
+//                 alert ('Winner!')
+//             } else {
+//                 alert ('try again(')
+//             }
+//         }
+//        }, 500)
+//       });
+//   });
+// }
+
+// function createPromise(delay) {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       const random = Math.random();
+//       if (random > 0.7) {
+//         resolve('✅');
+//       } else {
+//         reject('❌');
+//       }
+//     }, 1000 * delay);
+//   });
+// }
+
+//КАЗИНО ВАРІАНТ 2*******************************
+// *************************************************
+
+const start = document.querySelector('.btn');
+const container = document.querySelector('.container');
+
+start.addEventListener('click', onStart);
+
+function onStart() {
+  let counter = 0;
+
+  [...container.children].forEach(box => (box.textContent = ''));
+
+  const promises = [...container.children].map((_, i) => createPromise(i));
+
+  Promise.allSettled(promises).then(items => {
+    console.log(items);
+    items.forEach((item, i) => {
+      setTimeout(() => {
+        if (item.status === 'fulfilled') {
+          counter += 1;
+        }
+        console.log(item);
+        container.children[i].textContent = item.value || item.reason;
+
+        if (container.children.length - 1 === i) {
+          setTimeout(() => {
+            if (counter === container.children.length || !counter) {
+              alert('Winner');
+            } else {
+              alert('Lost money');
+            }
+          }, 500);
+        }
+      }, i * 1000);
+    });
+  });
+}
+
+function createPromise() {
+  return new Promise((resolve, reject) => {
+    const random = Math.random();
+    if (random > 0.7) {
+      resolve('✅');
+    } else {
+      reject('❌');
+    }
+  });
+}
